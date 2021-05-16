3772a733fc1318fc30cf1d98bccf43eba15a15d8
MatinMat8 <matinmat8.mat@gmail.com>
2021-05-16 23:33:53 +0430

    change and set up urls

--- Registration/urls.py
+++ Registration/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Registration/', include('register_login.urls', namespace='register_login')),
+    path('', include('django.contrib.auth.urls')),
     # path('accounts/', include('django.contrib.auth.urls')),
 ]
--- register_login/urls.py
+++ register_login/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from .views import *
 from django.contrib.auth import views as auth_views
@@ -8,5 +9,16 @@ urlpatterns = [
     path('', index, name='index'),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/Login.html')),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password.html'), name='password_reset'),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="registration/reset_password.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+
+    path('reset_password_completed/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_completed"),
 ]
